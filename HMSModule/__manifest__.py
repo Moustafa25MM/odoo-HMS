422971fe91a6d69476800bab5b90d0a255ac6262
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-03 16:55:30 +0200

    added patient model , view (lab 1)

---
+++ HMSModule/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':'hms',
+    'data':['views/hms_patient_view.xml',
+            # 'views/hms_doctor_view.xml',
+            # 'views/hms_department_view.xml'
+    ],
+    'license':'AGPL-3',
+    'Author':'Moustafa-Mahmoud'
+}
---
+++ HMSModule/models/hms_patient.py
@@ -0,0 +1,19 @@
+from odoo import models , fields , api
+
+class HMSPatient(models.Model):
+
+    _name = 'hms.patient'
+
+    First_name = fields.Char()
+    Last_name = fields.Char()
+    Birth_date = fields.Date()
+    History = fields.Html()
+    CR_Ratio = fields.Float()
+    Blood_type = fields.Selection ([('+a', '-a'),('+b', '-b'),('+c', '-c')])
+    PCR = fields.Boolean()
+    Image = fields.Image()
+    Address = fields.Text()
+    Age = fields.Integer()
+    
+
+   
