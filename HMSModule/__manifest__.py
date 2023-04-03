da89ae4832e10e245863269da9732bfd390d7452
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-03 17:04:49 +0200

    added views to data

--- HMSModule/__manifest__.py
+++ HMSModule/__manifest__.py
@@ -1,8 +1,8 @@
 {
     'name':'hms',
     'data':['views/hms_patient_view.xml',
-            # 'views/hms_doctor_view.xml',
-            # 'views/hms_department_view.xml'
+             'views/hms_doctor_view.xml',
+             'views/hms_department_view.xml'
     ],
     'license':'AGPL-3',
     'Author':'Moustafa-Mahmoud'
